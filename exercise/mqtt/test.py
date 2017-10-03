3d58b31fdddf678b76d6d25ef96099d4150a4589
Gregor von Laszewski <laszewski@gmail.com>
2017-10-03 07:55:44 -0400

    new:usr: update documentation for osx

--- exercise/mqtt/test.py
+++ exercise/mqtt/test.py
@@ -11,8 +11,8 @@ def on_message(client, userdata, message):
 def on_log(client, userdata, level, buf):
     print("log: ",buf)
 
-    
-broker_address="test.mosquitto.org"
+broker_address="localhost"    
+# broker_address="test.mosquitto.org"
 # broker_address="broker.hivemq.com"
 # broker_address="iot.eclipse.org"
 
